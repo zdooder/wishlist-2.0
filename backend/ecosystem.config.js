1063cfa39119f1b85d8da84e9f2b583d1eb4ca1e
Troy Morrison <troym@graphon.com>
2025-06-02 01:20:56 -0700

    pm2 config for backend deployment

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [{
+    name: 'wishlist-backend',
+    script: 'dist/server.js',
+    instances: 'max',
+    exec_mode: 'cluster',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }]
+} 

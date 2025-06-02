4bdfbe05e847ec85869a1e1490bd5442fe8cc064
Troy Morrison <troym@graphon.com>
2025-06-02 01:47:42 -0700

    configure vite server to proxy to backend for api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,14 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3001",
+        changeOrigin: true,
+        secure: false,
+        ws: false
+      }
+    }
   },
   build: {
     outDir: 'dist',
